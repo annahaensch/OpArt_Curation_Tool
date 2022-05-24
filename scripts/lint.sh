d812c5afb10d15995399162e0a353714d10f9560
Anna Haensch <annahaensch@gmail.com>
2022-05-24 11:24:22 -0400

    bring lint bash script back to date

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,13 +4,6 @@ set -ex # fail on first error, print commands
 
 SRC_DIR=${SRC_DIR:-$(pwd)}
 
-python -m pip install --upgrade pip
-pip install black
-pip install black[jupyter]
-pip install mypy==0.812
-pip install pytest
-pip install pytest-cov
-          
 echo "Checking code style with black..."
 python -m black --line-length 100 $(git ls-files '*.py')
 echo "Success!"
