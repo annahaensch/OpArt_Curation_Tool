c4a337f65c58469870ecf63c3918b282d4ea14ec
Anna Haensch <annahaensch@gmail.com>
2022-05-25 13:31:57 -0400

    lint setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
+""" Setup for OpArt library."""
+
 import codecs
 import os
 import re
 
 import setuptools
-from setuptools import setup, find_packages
-from setuptools.command.install import install
-from setuptools.command.develop import develop
 
 
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
@@ -15,32 +14,29 @@ VERSION_FILE = os.path.join(PROJECT_ROOT, "src", "__init__.py")
 
 
 def get_requirements():
+    """Buffer required dependencies."""
     with codecs.open(REQUIREMENTS_FILE) as buff:
         return buff.read().splitlines()
 
 
-def get_long_description():
-    with codecs.open(README_FILE, "rt") as buff:
-        return buff.read()
-
-
 def get_version():
-    lines = open(VERSION_FILE, "rt").readlines()
+    """Get version number."""
+    with open(VERSION_FILE, "rt", encoding="utf-8") as file:
+        lines = file.readlines()
     version_regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
     for line in lines:
-        mo = re.search(version_regex, line, re.M)
-        if mo:
-            return mo.group(1)
-    raise RuntimeError("Unable to find version in %s." % (VERSION_FILE,))
+        version = re.search(version_regex, line, re.M)
+        if version_regex:
+            return version.group(1)
+    raise RuntimeError(f"Unable to find version in {VERSION_FILE}.")
 
 
-setup(
+setuptools.setup(
     name="src",
     version=get_version(),
     description="",
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     install_requires=get_requirements(),
-    long_description=get_long_description(),
     long_description_content_type="text/markdown",
     include_package_data=True,
 )
